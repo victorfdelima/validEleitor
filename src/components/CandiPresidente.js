ab5b3366a165c64aaf1da596d9a0c0f80613b3d6
victorfdlima <vittinferreira@gmail.com>
2022-09-20 21:27:53 -0300

    regex-delete

--- src/components/CandiPresidente.js
+++ src/components/CandiPresidente.js
@@ -23,7 +23,7 @@ const CandiPresidente = ({ cand, loading }) => {
                                 </div>
                                 <div className="text-gray-600 text-xs">
                                     <div>
-                                        Votos: {item.vap.match(/((?:\d\.)?(?:\d{1,5}\.)?\d{1,5},\d{1,})|(\d)/g).join(".")}
+                                        Votos: {item.vap}
                                     </div>
                                 </div>
                                 <div className="ccN">
