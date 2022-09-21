01eb5eba1abc91da7ddd205b2210fba14aaaa4fb
victorfdlima <vittinferreira@gmail.com>
2022-09-20 23:07:10 -0300

    resolution-error

--- src/components/am/ApiPresidenteAM.js
+++ src/components/am/ApiPresidenteAM.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../../App.css";
-import APISenadorAP from "./ApiSenadorAP";
+import APISenadorAM from "./ApiSenadorAM";
 import CandiPresidenteAmazonas from "./CandiPresidenteAmazonas";
 import Pagination from "../Pagination";
 import APIGovernadorAM from "./APIGovernadorAM";
