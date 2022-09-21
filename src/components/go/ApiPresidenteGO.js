8fee7d95de73d3b134ac676805a64e4ff569a05b
Victor Lima <vittinferreira@gmail.com>
2022-09-21 12:40:37 -0300

    goiasadd

---
+++ src/components/go/ApiPresidenteGO.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import "../../App.css";
+import APISenadorGO from "./ApiSenadorGO";
+import CandiPresidenteGoias from "./CandiPresidenteGoias";
+import Pagination from "../Pagination";
+import APIGovernadorGO from "./APIGovernadorGO";
+import APIDeputadoFGO from "./APIDeputadoFGO";
+import APIDeputadoEGO from "./APIDeputadoEGO";
+
+const APIPresidenteGoias = () => {
+
+    const BASEURLPRESIDENTE = "https://resultados-sim.tse.jus.br/teste/ele2022/9240/dados-simplificados/go/go-c0001-e009240-r.json"
+    const [cand, setCand] = useState([]);
+    const [loading, setLoading] = useState(false)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [candPerPage] = useState(8)
+
+
+    useEffect(() => {
+        getUserPresidenteESanto();
+    }, []);
+    const getUserPresidenteESanto = async () => {
+        const api_response = await fetch(
+            `${BASEURLPRESIDENTE}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "cache-control": "s-maxage=10, stale-while-revalidate"
+                }
+            }
+        );
+
+
+        const cand = await api_response.json();
+        setCand(cand.cand);
+
+        setLoading(false)
+    };
+
+    // Current dados
+
+    const indexOfLastCand = currentPage * candPerPage;
+    const indexOfFirstCand = indexOfLastCand - candPerPage;
+    const currentCands = cand.slice(indexOfFirstCand, indexOfLastCand)
+
+    //Current Page
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    return (
+        <div>
+            <span className="titleAC">Presidentes</span>
+            <CandiPresidenteGoias cand={currentCands} loading={loading} />
+            <Pagination candPerPage={candPerPage} totalCands={cand.length} paginate={paginate} />
+            <div>
+                <div>
+                    <span className="titleAC">Governadores</span>
+                    <APIGovernadorGO cand={currentCands} loading={loading} />
+                </div>
+                <div>
+                    <span className="titleAC">Senadores</span>
+                    <APISenadorGO cand={currentCands} loading={loading} />
+                </div>
+                <div className="flex flex-row mt-4">
+                    <div className="flex-1">
+                        <span className="titleAC">Deputados Federais</span>
+                        <APIDeputadoFGO />
+                    </div>
+                </div>
+                <div className="flex flex-row mt-4">
+                    <div className="flex-1 ml-2">
+                        <span className="titleAC">Deputados Estaduais</span>
+                        <APIDeputadoEES />
+                    </div>
+                </div>
+            </div>
+            <div className="footer-dados">
+
+            </div>
+        </div>
+    );
+}
+export default APIPresidenteGoias
