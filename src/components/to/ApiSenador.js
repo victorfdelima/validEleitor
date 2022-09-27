4463efcb3a3be805a78f57aef93425be54aa0f36
victorfdelima <vittinferreira@gmail.com>
2022-09-26 21:30:51 -0300

    error-solution-part3

--- src/components/to/ApiSenador.js
+++ src/components/to/ApiSenador.js
@@ -13,28 +13,13 @@ const APISenador = () => {
 
 
     useEffect(() => {
-        getUserSenador();
-    }, []);
-    const getUserSenador = async () => {
-        const api_response = await fetch(
-            `${BASEURL}`,
-            {                
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "*",
-                    "cache-control": "s-maxage=10, stale-while-revalidate",
-                    "Access-Control-Allow-Methods": "GET",
-                    "Access-Control-Allow-Headers": "*",
-                }
-            }
-        );
-
-
-        const cand = await api_response.json();
-        setCand(cand.cand);
-        setLoading(false)
-    };
+        axios.get(BASEURL)
+        .then(response => {
+          setCand(response.data.cand)
+          setLoading(false)
+          console.log(response.data.cand)
+         }     )
+      },[])
 
     // Current dados
 
