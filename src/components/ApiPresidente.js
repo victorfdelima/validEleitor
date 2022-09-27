eea9666c1092b8d830dc159626e390c6141b3985
victorfdelima <vittinferreira@gmail.com>
2022-09-27 00:37:57 -0300

    testecors

--- src/components/ApiPresidente.js
+++ src/components/ApiPresidente.js
@@ -14,7 +14,6 @@ const APIPresidenteAll = () => {
 	 useEffect(() => {
 	     axios.get('https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json', {
         headers: {
-          'Content-Type': 'application/json',
           'cache-control': 's-maxage=50, stale-while-revalidate',
           'Access-Control-Allow-Origin': '*'
         }
